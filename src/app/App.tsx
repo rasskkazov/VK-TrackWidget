5fb9f8a5e2d47719424ceff122fd494b8c3f053e
Bogdan <brasskazov@gmail.com>
2024-06-23 03:47:57 +0300

    fix: remove extra dependencies

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
 import { RouterProvider } from "react-router-dom";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { AdaptivityProvider, ConfigProvider, AppRoot } from "@vkontakte/vkui";
 import "@vkontakte/vkui/dist/vkui.css";
 import { router } from "./router/Router";
 
-import { queryClient } from "@/shared/api/queryClient";
 import "./App.scss";
 
 export const App = () => {
@@ -12,9 +10,7 @@ export const App = () => {
     <ConfigProvider>
       <AdaptivityProvider>
         <AppRoot mode="partial">
-          <QueryClientProvider client={queryClient}>
-            <RouterProvider router={router} />
-          </QueryClientProvider>
+          <RouterProvider router={router} />
         </AppRoot>
       </AdaptivityProvider>
     </ConfigProvider>
