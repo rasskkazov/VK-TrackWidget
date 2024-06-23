70448e735fd13c9a43e37b907c2b716fee84b6bc
Bogdan <brasskazov@gmail.com>
2024-06-23 04:44:36 +0300

    fix:  path for gp

--- src/app/router/Router.tsx
+++ src/app/router/Router.tsx
@@ -14,6 +14,11 @@ export const router = createBrowserRouter([
         element: <Main />,
         errorElement: <ErrorPage />,
       },
+      {
+        path: paths.SEMI_MAIN,
+        element: <Main />,
+        errorElement: <ErrorPage />,
+      },
     ],
   },
 ]);
--- src/shared/constant/paths.ts
+++ src/shared/constant/paths.ts
@@ -1,3 +1,4 @@
 export enum paths {
   MAIN = "/",
+  SEMI_MAIN = "/VK-TrackWidget",
 }
